76ef79421c89689d103a6f4a86cecd06632013d5
rsthopaz <thopazrosadi15@gmail.com>
2026-05-19 21:05:54 +0700

    update service for graph

--- src/main/java/com/blockchain/analyzer/service/BPMNAnalysisService.java
+++ src/main/java/com/blockchain/analyzer/service/BPMNAnalysisService.java
@@ -1,15 +1,13 @@
 package com.blockchain.analyzer.service;
 
-import com.blockchain.analyzer.model.BPMNTask;
+import com.blockchain.analyzer.model.WorkflowGraph;
 import com.blockchain.analyzer.parser.BPMNParser;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class BPMNAnalysisService {
 
-    public List<BPMNTask> analyze(String filePath) {
+    public WorkflowGraph analyze(String filePath) {
 
         BPMNParser parser = new BPMNParser();
 
